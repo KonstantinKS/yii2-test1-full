4a041951cc8f501aca8d3054073dd415b4832284
KonstantinKS <k-karpov@inbox.ru>
2019-07-23 08:10:49 +0300

    ver 2019-08-23

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2_test1',
     'username' => 'demliz',
-    'password' => '1qa2WS3ed-',
+    'password' => 'demliz',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)

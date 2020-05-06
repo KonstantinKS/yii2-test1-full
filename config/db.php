1ceb687b1380c11c6de89362b4ada5423345a06e
KonstantinKS <k-karpov@inbox.ru>
2020-05-06 12:38:26 +0300

    Исправлена конфигурация базы данных
    [TW #24464140]

--- config/db.php
+++ config/db.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    //'class' => 'yii\db\Connection',
-    //'dsn' => 'mysql:host=localhost;dbname=yii2_test1',
-    //'username' => 'demliz',
-    //'password' => '1qa2WS3ed-',
-    //'charset' => 'utf8',
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2_test1',
+    'username' => 'demliz',
+    'password' => '1qa2WS3ed-',
+    'charset' => 'utf8',
 
     // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 60,
+    'schemaCache' => 'cache',
 ];
